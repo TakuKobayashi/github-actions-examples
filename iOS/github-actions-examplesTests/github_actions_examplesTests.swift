bf5c3d8cd469c70cfd597a77681574196c58ce05
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-10-16 15:34:26 +0900

    add iOS project.

---
+++ iOS/github-actions-examplesTests/github_actions_examplesTests.swift
@@ -0,0 +1,34 @@
+//
+//  github_actions_examplesTests.swift
+//  github-actions-examplesTests
+//
+//  Created by Taku Kobayashi on 2019/10/16.
+//  Copyright © 2019 TakuKobayashi. All rights reserved.
+//
+
+import XCTest
+@testable import github_actions_examples
+
+class github_actions_examplesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
