6ccff44f7c2ce3f909a2d78bf6fab60d20ba9260
Taku Kobayashi <keep_slimbody@yahoo.co.jp>
2019-11-04 22:32:12 +0900

    check deploy

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -4,5 +4,5 @@ import * as functions from 'firebase-functions';
 // // https://firebase.google.com/docs/functions/typescript
 //
 export const helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
+  response.send("Hello from Firebase!!!");
 });
