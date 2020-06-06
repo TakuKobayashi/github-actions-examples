f9b1d3a1297ffff429002f5be1e19eaf43470c8f
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2020-06-06 16:31:59 +0900

    change comment

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,8 +1,7 @@
 import { region, Request, Response } from "firebase-functions";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
+// When serve localhost, access below
+// http://localhost:5000/githubactionsexamples/asia-northeast1/helloWorld
 export const helloWorld = region("asia-northeast1").https.onRequest((request: Request, response: Response) => {
   response.send("Hello from Firebase!!!");
 });
