53f643c843a100ae979d797e7d4123c128e76e04
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2020-06-06 17:12:38 +0900

    add comment and change function name

--- serverless/handler.ts
+++ serverless/handler.ts
@@ -1,6 +1,9 @@
 import { APIGatewayProxyHandler, APIGatewayEvent, Context } from 'aws-lambda';
 import 'source-map-support/register';
 
+// When serverless offline start, access below
+// http://localhost:3000/dev/hello
+// add generate new AWS Lambda functions[hello]
 export const hello: APIGatewayProxyHandler = async (event: APIGatewayEvent, _context: Context) => {
   return {
     statusCode: 200,
@@ -21,6 +24,9 @@ app.get('/', (req, res) => {
   res.json({ hello: 'world' });
 });
 
+// When serverless offline start, access below
+// http://localhost:3000/dev/
+// add generate new AWS Lambda functions[api]
 export const api: APIGatewayProxyHandler = (event: APIGatewayEvent, context: Context) => {
   awsServerlessExpress.proxy(server, event, context);
 };
