92665b73ccb08f326fb7ce3af631c2fd89ef890c
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2020-06-06 16:38:45 +0900

    add express example

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,7 +1,19 @@
 import { region, Request, Response } from "firebase-functions";
+import * as express from 'express';
 
 // When serve localhost, access below
 // http://localhost:5000/githubactionsexamples/asia-northeast1/helloWorld
 export const helloWorld = region("asia-northeast1").https.onRequest((request: Request, response: Response) => {
   response.send("Hello from Firebase!!!");
 });
+
+const app = express();
+
+// express api sample
+app.get('/', (req, res) => {
+  res.json({ hello: 'world' });
+});
+
+// When serve localhost, access below
+// http://localhost:5000/githubactionsexamples/asia-northeast1/api
+export const api = region('asia-northeast1').https.onRequest(app);
