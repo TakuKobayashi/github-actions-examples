5509e49110d58d14cc871ec20260d23dabc417da
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2025-06-24 00:55:56 +0900

    new iOS Xcode project.

---
+++ iOS/github-actions-examples/github_actions_examplesApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct github_actions_examplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOS/github-actions-examplesTests/github_actions_examplesTests.swift
@@ -0,0 +1,10 @@
+import Testing
+@testable import github_actions_examples
+
+struct github_actions_examplesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
